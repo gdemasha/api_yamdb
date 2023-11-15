a3721bba8c36f0f8f3fa901e3d945c64ffd53e86
Андрей Хлиманенков <hlimanenkov@gmail.com>
2023-11-15 16:07:30 +0300

    удалено лишнее из приложения reviews, оставлены только модели

--- api_yamdb/api_yamdb/urls.py
+++ api_yamdb/api_yamdb/urls.py
@@ -1,10 +1,9 @@
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path
 from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/', include('reviews.urls')),
     path(
         'redoc/',
         TemplateView.as_view(template_name='redoc.html'),
