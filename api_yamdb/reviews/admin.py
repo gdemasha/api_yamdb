033e7737e97aa60e147e497b69a1fbb07e50d80d
Мария Медникова <maari.mednikova@yandex.ru>
2023-11-24 12:44:34 +0300

    Исправленная стилистика кода

--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from reviews.models import Category, User, Genre, Review, Title
+from reviews.models import Category, Genre, Review, Title, User
 
 
 @admin.register(Title)
